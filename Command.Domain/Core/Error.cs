3dae86cb49c3265b318b4367e6cc64882b70650a
Pedro <36316038+zpdh@users.noreply.github.com>
2025-02-06 20:05:43 -0300

    feat: start command domain project

---
+++ Command.Domain/Core/Error.cs
@@ -0,0 +1,15 @@
+namespace Command.Domain.Core;
+
+public class Error(string errorMessage)
+{
+    public readonly string ErrorMessage = errorMessage;
+
+    public static readonly Error None = new(string.Empty);
+    public static readonly Error NullArgument = new("The specified value is null.");
+    public static readonly Error ConditionNotMet = new("The specified condition was not met.");
+
+    public static implicit operator Result(Error error)
+    {
+        return Result.Failure(error);
+    }
+}
