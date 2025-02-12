0be2fc5b0453e309c8edcac96e68eec1ac6552e0
Pedro <36316038+zpdh@users.noreply.github.com>
2025-02-12 16:39:20 -0300

    feat: add POST endpoint to test connection

--- Command/Command.Api/Controllers/PlatformsController.cs
+++ Command/Command.Api/Controllers/PlatformsController.cs
@@ -1,9 +1,16 @@
 ﻿using Command.Api.Core;
 using Command.App.Core.Messaging.Abstractions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Command.Api.Controllers;
 
 public class PlatformsController(IMediator mediator) : ApiController(mediator)
 {
-    
+    [HttpPost]
+    public IActionResult TestConnection()
+    {
+        Console.WriteLine("Connection @ /platforms POST");
+
+        return Ok("Commands Service connection test");
+    }
 }
