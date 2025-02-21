d59c63ac6710bb900767a1ec976aafc9555cb69b
Pedro <36316038+zpdh@users.noreply.github.com>
2025-02-20 21:52:37 -0300

    feat: add command and platform entities

--- Command/Command.Domain/Command/Command.cs
+++ Command/Command.Domain/Command/Command.cs
@@ -1,8 +1,23 @@
-﻿using Command.Domain.Core;
+﻿using System.Text.Json.Serialization;
+using Command.Domain.Core;
 
 namespace Command.Domain.Command;
 
 public class Command : Entity
 {
+  public int PlatformId { get; set; }
+  public string HowTo { get; set; } = string.Empty;
+  public string CommandLine { get; set; } = string.Empty;
+  [JsonIgnore] public Platform.Platform Platform { get; set; } = default!;
 
+  public Command(string howTo, string commandLine)
+  {
+    HowTo = howTo;
+    CommandLine = commandLine;
+  }
+
+  private Command()
+  {
+
+  }
 }
---
+++ Command/Command.Domain/Platform/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using Command.Domain.Core;
+
+namespace Command.Domain.Platform;
+
+public sealed class Platform : Entity
+{
+  public string Name { get; set; } = string.Empty;
+  [JsonIgnore] public ICollection<Command.Command> Commands { get; set; } = [];
+
+  public Platform(string name)
+  {
+    Name = name;
+  }
+
+  private Platform() { }
+}
+
